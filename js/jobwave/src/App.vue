0abd8ab870e3f3ddc9ba40935517dcaa9cb14fd4
Chitra Limbu <chitra-home@niku-macbook.home>
2022-08-24 23:14:11 +0100

    Completed frontend html

---
+++ js/jobwave/src/App.vue
@@ -0,0 +1,140 @@
+<template>
+  <!--  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>-->
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+      <!-- Toggle button -->
+      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse">
+        <i class="fas fa-bars"></i>
+      </button>
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Navbar brand -->
+        <a class="navbar-brand mt-2 mt-lg-0" href="#">
+          <img
+            id="main-logo"
+            src="../public/img/jobwavelogo.jpeg"
+            alt="main-logo"
+            loading="lazy"
+          />
+        </a>
+      </div>
+      <!-- Right elements -->
+      <div class="d-flex align-items-center">
+        <!-- Icon -->
+        <a class="text-reset me-3" href="#">
+          <i class="fas fa-shopping-cart"></i>
+        </a>
+
+        <!-- Notifications -->
+        <div class="dropdown" id="logo-text">Trabalho Temporario lda</div>
+      </div>
+      <!-- Right elements -->
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+  <nav class="navbar navbar-expand-lg second-nav">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+      <!-- Toggle button -->
+      <button class="navbar-toggler" type="button">
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent_two">
+        <!-- Navbar brand -->
+        <a class="navbar-brand mt-2 mt-lg-0 second-nav-text" href="#"> Jobs </a>
+        <a class="navbar-brand mt-2 mt-lg-0 second-nav-text" href="#">
+          Upload CV
+        </a>
+      </div>
+      <!-- Collapsible wrapper -->
+
+      <!-- Right elements -->
+      <div class="d-flex align-items-center">
+        <!-- Icon -->
+        <a class="text-reset me-3" href="#">
+          <i class="fas fa-shopping-cart"></i>
+        </a>
+
+        <!-- Notifications -->
+        <div class="dropdown" id="contact-logo">
+          <a href="tel:+447477022455"
+            ><i class="bi bi-telephone-fill contact-item"></i
+          ></a>
+          <a href="mailto:geral.jobwave@gamil.com"
+            ><i class="bi bi-envelope-fill contact-item"></i
+          ></a>
+          <a href="#">
+            <i class="bi bi-instagram contact-item"></i>
+          </a>
+          <a href="#">
+            <i class="bi bi-facebook contact-item"></i>
+          </a>
+          <a href="#">
+            <i class="bi bi-twitter contact-item"></i>
+          </a>
+        </div>
+      </div>
+      <!-- Right elements -->
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+  <!-- Navbar -->
+  <router-view />
+</template>
+
+<style scoped>
+#main-logo {
+  width: 70px;
+  margin-left: 100px;
+}
+
+#logo-text {
+  margin-right: 100px;
+  font-weight: bold;
+  color: #5a5a5aff;
+  font-size: 27px;
+  font-family: "Trebuchet MS", sans-serif;
+}
+
+#navbarSupportedContent_two {
+  margin-left: 70px;
+}
+
+#contact-logo {
+  margin-right: 100px;
+}
+
+.contact-item {
+  color: white;
+  margin-left: 40px;
+  font-size: 20px;
+}
+
+.second-nav {
+  background-color: rgb(27, 199, 255);
+  padding: 0;
+}
+
+.second-nav-text {
+  width: 100px;
+  text-align: center;
+  font-size: 16px;
+  font-family: "Trebuchet MS", sans-serif;
+  color: white;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin: 0;
+}
+
+.second-nav-text:hover {
+  color: white !important;
+  background-color: rgb(27, 175, 255);
+}
+</style>
---
+++ js/jobwave/src/views/Home.vue
@@ -0,0 +1,181 @@
+<template>
+  <div id="container">
+    <div id="wrapper">
+      <div id="form-container">
+        <div id="form-wrapper">
+          <form>
+            <div class="row">
+              <div id="tell" class="col">Tell us about yourself ...</div>
+            </div>
+            <div class="row g-3">
+              <div class="col">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="First name"
+                  aria-label="First name"
+                  required
+                />
+              </div>
+              <div class="col">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Last name"
+                  aria-label="Last name"
+                  required
+                />
+              </div>
+            </div>
+            <div class="row g-3">
+              <div class="col">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Phone Number"
+                  aria-label="Phone Number"
+                  pattern="[0-9].*"
+                  required
+                />
+              </div>
+              <div class="col">
+                <input
+                  type="email"
+                  class="form-control"
+                  placeholder="my@email.com"
+                  aria-label="Email"
+                  required
+                />
+              </div>
+            </div>
+            <div class="row g-3">
+              <div class="col">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="More Details"
+                  aria-label="More Details"
+                />
+              </div>
+              <div class="col">
+                <input
+                  class="form-control"
+                  type="file"
+                  id="formFile"
+                  required
+                />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <button id="submit-form" type="submit" class="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="bottom-container">
+    <div id="bottom-wrapper">
+      <div class="row row-cols-8 bottom-grid">
+        <div class="col">housekeeping</div>
+        <div class="col">waiter</div>
+        <div class="col">cook</div>
+        <div class="col">cleaner</div>
+        <div class="col">agriculture</div>
+        <div class="col">butler</div>
+        <div class="col">cruise</div>
+        <div class="col">many more</div>
+      </div>
+    </div>
+  </div>
+
+  <div id="footer-container">
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "@/components/Footer";
+
+export default {
+  name: "Home",
+  components: { Footer },
+};
+</script>
+
+<style scoped>
+#container {
+  width: 100%;
+}
+
+#wrapper {
+  position: relative;
+  width: 100%;
+  background-image: url("../../public/img/work.jpg");
+  background-size: cover;
+  background-color: skyblue;
+  height: 70vh;
+}
+
+#bottom-container {
+  width: 100%;
+  height: 100%;
+}
+
+#bottom-wrapper {
+  width: 100%;
+  background-color: rgb(27, 199, 255);
+  height: 10.9vh;
+}
+
+#form-container {
+  height: 80%;
+  background-color: white;
+  width: 60vw;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+#submit-form {
+  width: 100%;
+  border: none;
+  background-color: rgb(27, 175, 255);
+}
+
+.col {
+  padding-top: 25px;
+  font-family: "Trebuchet MS", sans-serif;
+}
+
+#form-wrapper {
+  width: 85%;
+  margin: 0;
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+#tell {
+  font-size: 40px;
+}
+
+.bottom-grid {
+  color: white;
+  text-align: center;
+  margin-left: 70px;
+  margin-right: 100px;
+}
+
+#footer-container {
+  max-width: 100vw;
+}
+</style>
